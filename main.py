33c3a66a38391ff3a22704d3cd9d6a6314652cd6
Quentin Romero Lauro <60079836+QuentinRomeroLauro@users.noreply.github.com>
2022-10-10 11:32:15 -0400

    added comments

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# takes in String name input
 name = input("What's your name?")
+
+# takes in String color input
 color = input("What's your favorite color")
+
+# prints name in color in sentence
 print("Your name is", name, "and your favorite color is", color)
 
